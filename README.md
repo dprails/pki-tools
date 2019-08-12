36a4835c68cc5f22a6a51878e1e7362fa356415c
Mark Mastoras <mmastoras@mac.com>
2019-08-12 15:04:35 -0700

    make more useful

--- README.md
+++ README.md
@@ -4,8 +4,7 @@ Docker image containing handy script for creating TLS keys. These keys should be
 
 # Creating TLS keys
 1. shell into container
-`$ docker run -it dprails/pki-tools /bin/bash`
+`$ docker run -it -v $(pwd):/pki-tools/pki dprails/pki-tools /bin/bash`
 2. run script
 `# ./create_pki_keys.sh`
-3. keys created in pki directory
-4. copy keys out of container to use as needed
+3. keys created in  your host's $(pwd)/pki directory
--- create_pki_keys.sh
+++ create_pki_keys.sh
@@ -2,11 +2,17 @@
 
 set -e
 
+if [ $1 == "--help" ] || [ $1 == "-h"]
+then
+  echo "Usage: $0 [--s3]" >&2
+  exit 1
+fi
+
 ### assumes openssl is installed, that we are running on linux variant, and example-pki-scripts have been installed local
 ### to current directory
 
 echo "creating pki directory"
-mkdir -p pki/admin
+mkdir -p pki
 cd example-pki-scripts
 ca_password="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)"
 echo $ca_password > ../pki/ca_password.txt
@@ -33,18 +39,12 @@ mv $node_dn* ../pki/$node_dn/.
 cp ../pki/$node_dn/$node_dn.crt.pem ../pki/node.pem
 cp ../pki/$node_dn/$node_dn.key ../pki/node.key
 
-echo "generating admin certificate"
-admin_password="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)"
-echo $admin_password > ../pki/admin_password.txt
-export set DN="CN=admin,OU=IT,O=$org_name,L=San Francisco,C=US"
-./gen_client_node_cert.sh admin $admin_password $ca_password
-mv admin* ../pki/admin/.
-cp ../pki/admin/admin-keystore.jks ../pki/admin-keystore.jks
-
-#echo "pki keys generated and stored in pki directory, uploading these to S3 for safe keeping. Enter the s3 bucket and path to upload to:"
-#cd ../
-#read s3_bucket_path
-#echo "uploading pki keys to s3://$s3_bucket_path/pki..."
-#aws s3 cp pki/ s3://$s3_bucket_path/ --recursive
-#echo "uploading root ca to s3://$s3_bucket_path/pki/ca..."
-#aws s3 cp search-guard-ssl/example-pki-scripts/ca/ s3://$s3_bucket_path/ca/ --recursive
+if [ "$1" == "--s3" ]; then
+  echo "pki keys generated and stored in pki directory, uploading these to S3 for safe keeping. Enter the s3 bucket and path to upload to:"
+  cd ../
+  read s3_bucket_path
+  echo "uploading pki keys to s3://$s3_bucket_path/pki..."
+  aws s3 cp pki/ s3://$s3_bucket_path/ --recursive
+  echo "uploading root ca to s3://$s3_bucket_path/pki/ca..."
+  aws s3 cp search-guard-ssl/example-pki-scripts/ca/ s3://$s3_bucket_path/ca/ --recursive
+fi
